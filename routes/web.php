86af4999f71bc1022f2a0f2034bf3c141223f81e
Your Name <you@example.com>
2024-12-10 19:22:28 +0500

    комент

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
 Route::get('/create-skill', [SkillController::class, 'renderCreatePage'])
     ->middleware('auth')
     ->name('skillCreate');
-
+//(помогите пожалуйста)
 Route::post('/create-skill', [SkillController::class, 'createSkill'])
     ->middleware('auth')
     ->name('skillCreate.post');
